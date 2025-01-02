d36b0c478b65473be3b687e53654a21100da1c63
Greg Thorson <Engywook2005@gmail.com>
2025-01-02 13:46:10 -0500

    Cleaned up tabs in medscapeBidAdapter

--- modules/medscapeBidAdapter.js
+++ modules/medscapeBidAdapter.js
@@ -3,21 +3,21 @@ import {registerBidder} from '../src/adapters/bidderFactory.js';
 const BIDDER_CODE = 'medscape';
 
 const spec = {
-    code: BIDDER_CODE,
-    isBidRequestValid: (bid) => {
-        return !!bid.params.placementId;
-    },
-    buildRequests: (validBidRequests, bidderRequest) => {
-        debugger;
-        // See what happens here
-        // send bid request to medscape
-    },
-    interpretResponse: (serverResponse, request) => {
-        debugger;
-        // again see what happens
-    },
-    getUserSyncs: () => {
-        // Do we need this?
-        return [];
-    }
+  code: BIDDER_CODE,
+  isBidRequestValid: (bid) => {
+    return !!bid.params.placementId;
+  },
+  buildRequests: (validBidRequests, bidderRequest) => {
+    debugger;
+    // See what happens here
+    // send bid request to medscape
+  },
+  interpretResponse: (serverResponse, request) => {
+    debugger;
+    // again see what happens
+  },
+  getUserSyncs: () => {
+    // Do we need this?
+    return [];
+  }
 }
